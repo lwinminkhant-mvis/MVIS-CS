f097b3ae4da3b728dfc4a831d15271ae8808b202
lwinminkhant-mvis <lwinminkhant@mvis.school>
2026-05-04 14:55:29 +0630

    Create key_for_database.py

---
+++ computer_science/key_for_database.py
@@ -0,0 +1,11 @@
+a = False
+
+while a != True:
+    if word != 2:
+        print("the word is not 2 letter long!")
+        word = input("give me 2 letter word: ")
+    else:
+        a = True
+
+    
+print("yss")

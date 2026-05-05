dfb54978b7548b3931ffe2c6c4b73418790a2cec
lwinminkhant-mvis <lwinminkhant@mvis.school>
2026-05-05 15:09:59 +0630

    Update key_for_database.py

--- computer_science/key_for_database.py
+++ computer_science/key_for_database.py
@@ -2,7 +2,6 @@ import os
 os.system('cls')
 
 
-
 two_letter_word = input('give 2 letter word: ')
 
 while len(two_letter_word) != 2:
@@ -13,7 +12,7 @@ while len(two_letter_word) != 2:
 
 whole_number = input('give an whole number: ')
 
-decimal_number = str(int(float(input('give an decimal_number'))))
+decimal_number = str(int(float(input('give an decimal_number: '))))
 
 
 print(whole_number + two_letter_word[1] + two_letter_word[0] + decimal_number)
